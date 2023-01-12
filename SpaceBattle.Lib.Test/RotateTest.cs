00bf69d7d5c86753e87ff5d7e29e21dd8a9f84f3
Ekaterina <kate_efimova@inbox.ru>
2023-01-12 16:03:51 +0600

    Добавленны тесты 2 и 3

--- SpaceBattle.Lib.Test/RotateTest.cs
+++ SpaceBattle.Lib.Test/RotateTest.cs
@@ -15,4 +15,35 @@ public class RotateTest
 
         Assert.Equal(135, obj.Object.Angle);
     }
+    [Fact]
+    public void Test2()
+    {
+        Mock<IRotatable> obj = new();
+        obj.SetupGet(x => x.Angle).Throws<Exception>();
+        RotateCommand rotate = new(obj.Object);
+        Assert.Throws<Exception>(()=>rotate.Execute());
+
+    }
+    [Fact]
+    public void Test3()
+    {
+        
+        Mock<IRotatable> obj = new();
+        obj.SetupProperty(x => x.Angle, 45);
+        obj.SetupGet(x => x.AngleSpeed).Throws<Exception>();
+        RotateCommand rotate = new(obj.Object);
+        Assert.Throws<Exception>(()=>rotate.Execute());
+
+    }
+    [Fact]
+    public void Test4()
+    {
+        Mock<IRotatable> obj = new();
+        obj.SetupGet(x => x.Angle).Returns(0);
+        obj.SetupSet(x => x.Angle = It.IsAny<double>()).Throws(new Exception());
+        obj.SetupGet(x => x.AngleSpeed).Returns(90);
+        RotateCommand rotate = new(obj.Object);
+
+        Assert.Throws<Exception>(rotate.Execute);
+    }
 }

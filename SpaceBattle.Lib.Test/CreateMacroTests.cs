0c42fa8e3b899f152042ced614e831d807ab76df
Egor <egor951769794@gmail.com>
2023-01-12 19:53:10 +0600

    более солидный тест макрокоманды

--- SpaceBattle.Lib.Test/CreateMacroTests.cs
+++ SpaceBattle.Lib.Test/CreateMacroTests.cs
@@ -13,7 +13,7 @@ public class CreateMacroTests
 
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.GetDependantCommandNames", (object[] args) => new List<string> {"Dependant_Command"}).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Dependant_Command", (object[] args) => new Mock<SpaceBattle.Lib.ICommand>().Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.GetCommand.WithArgs", (object[] args) => new Mock<IStrategy>().Object).Execute();
     }
     [Fact]
     public void successfulCreateMacro()
@@ -21,4 +21,12 @@ public class CreateMacroTests
         var newMacro = new Mock<CreateMacro>();
         newMacro.Object.Run(new object[] {It.IsAny<string>(), new object()});
     }
+    // [Fact]
+    // public void unsuccessfulCreateMacroNameThrowsException()
+    // {
+    //     var newMacro = new Mock<CreateMacro>();
+    //     var cmdName = new Mock<List<string>>();
+    //     cmdName.SetupGet(x => x[It.IsAny<int>()]).Throws<Exception>();
+    //     Assert.Throws<Exception>(() => newMacro.Object.Run(new object[] {cmdName.Object[0], new object()}));
+    // }
 }

1798e6046e5fd1d90447ca870a5faf801d63e58d
Egor <egor951769794@gmail.com>
2023-04-18 10:02:26 +0600

    фикс тестов?

--- SpaceBattle.Lib.Test/HardStopTests.cs
+++ SpaceBattle.Lib.Test/HardStopTests.cs
@@ -98,5 +98,7 @@ public class HardStopTests
         IoC.Resolve<ICommand>("Threading.SendCommand", 1, releaseThread).Execute();
 
         Assert.False(objToMove.Object.position == new Vector(5, 8));
+
+        waiter.WaitOne();
     }
 }

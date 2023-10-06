fc76b0f78bebd9a1723801ec808169e212d4b13e
Ekaterinaef14 <115913381+Ekaterinaef14@users.noreply.github.com>
2023-10-06 10:27:55 +0600

    my first proba

---
+++ ExceptionHandlerStrategy.cs
@@ -0,0 +1,23 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class ExceptionLogCommand : ICommand
+{   
+    ICommand command;
+    Exception ex;
+    public ExceptionLogCommand(ICommand command, Exception ex)
+    {
+        this.command = command;
+        this.ex = ex;
+    }
+    public void Execute()
+    {
+        string logFileName = IoC.Resolve<string>("Exception.GetLogName");
+        string errorMessage = $"Error in command '{command.GetType().Name}': {ex.Message}";
+
+        using (StreamWriter writer = new StreamWriter(logFileName, true))
+        {
+            writer.WriteLine(errorMessage);
+        }
+    }
+}

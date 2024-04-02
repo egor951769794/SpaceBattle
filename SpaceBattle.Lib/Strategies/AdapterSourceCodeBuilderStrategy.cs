813390c9eea6fe9ffd7b747abd76d0f99313d3d3
Ekaterinaef14 <kate_efimova@inbox.ru>
2024-04-02 11:16:49 +0600

    исправление 1 пробы лабы

--- SpaceBattle.Lib/Strategies/AdapterSourceCodeBuilderStrategy.cs
+++ SpaceBattle.Lib/Strategies/AdapterSourceCodeBuilderStrategy.cs
@@ -3,14 +3,14 @@ namespace SpaceBattle.Lib.Strategies;
 using System.Linq;
 using System.Reflection;
 using SpaceBattle.Lib;
-using SpaceBattle.Collections;
+using SpaceBattle.General;
 
 public class AdapterSourceCodeBuilderStrategy : IStrategy
 {
     public object Run(params object[] argv)
     {
         var dtype = (Type)argv[0];
-        var builder = Container.Resolve<IBuilder>("Entities.Builders.AdapterSourceCode").Config("Dtype", dtype);
+        var builder = Container.Resolve<IBuilder>("AdapterSourceCode").Config("Dtype", dtype);
 
         foreach (PropertyInfo prop in dtype.GetProperties())
         {

075c4a3d542c3b1eefe235ae5399518018140609
Egor <egor951769794@gmail.com>
2024-12-10 17:19:27 +0600

    v3.1

--- SpaceBattle.Lib.Test/ShardedThreadsTests.cs
+++ SpaceBattle.Lib.Test/ShardedThreadsTests.cs
@@ -8,12 +8,12 @@ namespace SpaceBattle.Lib.Test;
 
 public class ShardedThreadsTests
 {
-
+    int numberOfGames;
     public ShardedThreadsTests()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
-        int numberOfGames = 0;
+        numberOfGames = 1;
 
         ConcurrentDictionary<string, ConcurrentQueue<SpaceBattle.Lib.ICommand>> gamesQueues = new ConcurrentDictionary<string, ConcurrentQueue<SpaceBattle.Lib.ICommand>>();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.GetAll", (object[] args) => gamesQueues).Execute();
@@ -21,16 +21,17 @@ public class ShardedThreadsTests
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.MakeNewId", (object[] args) => "game" + numberOfGames++.ToString()).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.GameCommand", (object[] args) => new GameCommand(args[0], (IEnumerable<ICommand>) args[1])).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.InterpretMessage", (object[] args) => new InterpretThreadMessageCommand((IMessage) args[0])).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CreateCommand", (object[] args) => new Mock<ICommand>().Object).Execute();
     }
     
     [Fact]
-    public void test1()
+    public void GameCreationTest()
     {
+        numberOfGames = 1;
         BlockingCollection<ICommand> gameQueue1 = new BlockingCollection<ICommand>();
         BlockingCollection<ICommand> gameQueue2 = new BlockingCollection<ICommand>();
 
         BlockingCollection<ICommand> msgQueue1 = new BlockingCollection<ICommand>();
-        // Mock<BlockingCollection<ICommand>> mockMsgQueue1 = new Mock<BlockingCollection<ICommand>>();
         BlockingCollection<ICommand> msgQueue2 = new BlockingCollection<ICommand>();
 
         ThreadMessageSenderAdapter msa1 = new ThreadMessageSenderAdapter(msgQueue1);
@@ -60,24 +61,44 @@ public class ShardedThreadsTests
         new CreateNewGameConcurrent().Run();
         
         Assert.Equal(1, IoC.Resolve<ConcurrentDictionary<string, ConcurrentQueue<SpaceBattle.Lib.ICommand>>>("Game.Queue.GetAll").Count);
-        Assert.Equal(0, IoC.Resolve<ConcurrentDictionary<string, ConcurrentQueue<SpaceBattle.Lib.ICommand>>>("Game.Queue.GetAll")["game0"].Count);
+        Assert.Equal(0, IoC.Resolve<ConcurrentQueue<SpaceBattle.Lib.ICommand>>("Game.Queue.Get", "game1").Count);
 
         new CreateNewGameConcurrent().Run();
 
-        Assert.Equal(0, IoC.Resolve<ConcurrentDictionary<string, ConcurrentQueue<SpaceBattle.Lib.ICommand>>>("Game.Queue.GetAll")["game1"].Count);
+        Assert.Equal(0, IoC.Resolve<ConcurrentQueue<SpaceBattle.Lib.ICommand>>("Game.Queue.Get", "game2").Count);
         Assert.Equal(2, IoC.Resolve<ConcurrentDictionary<string, ConcurrentQueue<SpaceBattle.Lib.ICommand>>>("Game.Queue.GetAll").Count);
 
+    }
+    [Fact]
+    public void MessageSendingTest()
+    {
+        numberOfGames = 1;
+        BlockingCollection<ICommand> gameQueue1 = new BlockingCollection<ICommand>();
+
+        BlockingCollection<ICommand> msgQueue1 = new BlockingCollection<ICommand>();
+
+        ThreadMessageSenderAdapter msa1 = new ThreadMessageSenderAdapter(msgQueue1);
+
+        Dictionary<string, ThreadMessageSenderAdapter> msgSenders = new Dictionary<string, ThreadMessageSenderAdapter>();
+        msgSenders["th1"] = msa1;
+
+        ReceiverAdapter gra1 = new ReceiverAdapter(gameQueue1);
+
+        ReceiverAdapter mra1 = new ReceiverAdapter(msgQueue1);
+
+        ServerThread thread1 = new ServerThread(gra1, mra1);
+
+        List<ServerThread> threads = new List<ServerThread> {thread1};
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Threading.Thread.Get", (object[] args) => threads[(int) args[0]]).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Threading.Get.MessageSender", (object[] args) => msgSenders[(string) args[0]]).Execute();
+
+        ConcurrentDictionary<string, List<string>> threadsGames = new ConcurrentDictionary<string, List<string>>();
+        threadsGames["th1"] = new List<string>();
+
+        new CreateNewGameConcurrent().Run();
 
         threadsGames["th1"].Add("game1");
-        threadsGames["th2"].Add("game2");
-
-        // Mock<IMessage> mockMsg = new Mock<IMessage>();
-        // mockMsg.Setup(x => x.Gameid).Returns("game0");
-        // Mock<IStrategy> mockStrat = new Mock<IStrategy>();
-        // mockStrat.Setup(x => x.Run("th1", mockMsg.Object)).Verifiable();
-
-
-        // mockStrat.Verify();
 
         Mock<IMessage> mockMsg = new Mock<IMessage>();
         mockMsg.Setup(x => x.Gameid).Returns("game1");
@@ -87,6 +108,47 @@ public class ShardedThreadsTests
         shThreads.sendMessage(mockMsg.Object);
 
         Assert.Equal(1, msgQueue1.Count);
-        Assert.Equal(0, msgQueue2.Count);
+    }
+    [Fact]
+    public void MessageInterpretationTest()
+    {
+        numberOfGames = 1;
+        BlockingCollection<ICommand> gameQueue1 = new BlockingCollection<ICommand>();
+
+        BlockingCollection<ICommand> msgQueue1 = new BlockingCollection<ICommand>();
+
+        ThreadMessageSenderAdapter msa1 = new ThreadMessageSenderAdapter(msgQueue1);
+
+        Dictionary<string, ThreadMessageSenderAdapter> msgSenders = new Dictionary<string, ThreadMessageSenderAdapter>();
+        msgSenders["th1"] = msa1;
+
+        ReceiverAdapter gra1 = new ReceiverAdapter(gameQueue1);
+
+        ReceiverAdapter mra1 = new ReceiverAdapter(msgQueue1);
+
+        ServerThread thread1 = new ServerThread(gra1, mra1);
+
+        List<ServerThread> threads = new List<ServerThread> {thread1};
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Threading.Thread.Get", (object[] args) => threads[(int) args[0]]).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Threading.Get.MessageSender", (object[] args) => msgSenders[(string) args[0]]).Execute();
+
+        ConcurrentDictionary<string, List<string>> threadsGames = new ConcurrentDictionary<string, List<string>>();
+        threadsGames["th1"] = new List<string>();
+
+        new CreateNewGameConcurrent().Run();
+
+        threadsGames["th1"].Add("game1");
+
+        Mock<IMessage> mockMsg = new Mock<IMessage>();
+        mockMsg.Setup(x => x.Gameid).Returns("game1");
+
+        ShardedThreadsMessagesInterpreter shThreads = new ShardedThreadsMessagesInterpreter(threadsGames);
+
+        shThreads.sendMessage(mockMsg.Object);
+
+        msgQueue1.Take().Execute();
+
+        Assert.Equal(1, IoC.Resolve<ConcurrentDictionary<string, ConcurrentQueue<SpaceBattle.Lib.ICommand>>>("Game.Queue.GetAll")["game1"].Count);
     }
 }
